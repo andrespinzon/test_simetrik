01708f16c95fc272abd0964db35606333df0388b
andres <inge.andrespinzon@gmail.com>
2020-11-21 08:55:21 -0500

    Feat | add config ORM db and external db

---
+++ config/extension.py
@@ -0,0 +1,15 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+    os.environ.get(
+        'DB_EXTERNAL'
+    )
+)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+session = Session()
